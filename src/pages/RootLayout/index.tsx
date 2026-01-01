cdc60d87af36dfba69bccd2903be0117b0a3086d
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-01-01 15:35:22 -0300

    ✨ feat: inicializa autenticação global no layout raiz
    Adiciona a inicialização do fluxo de autenticação no layout raiz,
    disparando o thunk de initAuth ao montar a aplicação.
    Essa abordagem garante que a sessão do usuário seja carregada e
    sincronizada com o estado global do Redux desde o início da navegação.

--- src/pages/RootLayout/index.tsx
+++ src/pages/RootLayout/index.tsx
@@ -1,9 +1,19 @@
+import { useEffect } from 'react'
 import { Outlet } from 'react-router'
 import { ToastContainer } from 'react-toastify'
+import { useDispatch } from 'react-redux'
+import { initAuth } from '@/app/store/auth/authThunks'
 import { Container } from './styles'
+import type { AppDispatch } from '@/app/store'
 import Header from '@/components/Header'
 
 const RootLayout = () => {
+    const dispatch = useDispatch<AppDispatch>()
+
+    useEffect(() => {
+        dispatch(initAuth())
+    }, [dispatch])
+
     return (
         <>
             <ToastContainer />
