fe012086c26265c568320943d67e24623ae1aac3
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2026-01-01 16:54:44 -0300

    ✨ feat: adiciona lista de ações para usuários não autenticados
    Cria o componente UnauthenticatedActionList para exibir ações de cadastro e login quando o usuário não está autenticado.
    Essa separação melhora a organização do Header e deixa o controle de estado de autenticação mais claro na interface.

---
+++ src/components/Header/UnauthenticatedActionList/index.tsx
@@ -0,0 +1,21 @@
+import { List, ListItem } from '../styles'
+import Button from '@/components/Button'
+
+const UnauthenticatedActionList = () => {
+    return (
+        <List>
+            <ListItem>
+                <Button link to="/auth/register">
+                    Abrir conta
+                </Button>
+            </ListItem>
+            <ListItem>
+                <Button outline link to="/auth/login">
+                    Login
+                </Button>
+            </ListItem>
+        </List>
+    )
+}
+
+export default UnauthenticatedActionList
